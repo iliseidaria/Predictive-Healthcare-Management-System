2bbca5200f2b2761804547db2d685a64991c6db5
Pîntea Andreea <andreea.pinte4@gmail.com>
2024-11-03 17:53:56 +0200

    application added

--- Domain/Repositories/IPatientRepository.cs
+++ Domain/Repositories/IPatientRepository.cs
@@ -5,7 +5,7 @@ public interface IPatientRepository
 {
     Task<List<Patient>> GetAllPatientsAsync();
     Task<Patient> GetPatientByIdAsync(Guid patientId);
-    Task<bool> AddAsync(Patient patient);
     Task<bool> UpdateAsync(Patient patient);
-    Task<bool> DeleteAsync(Guid patientId);
+    Task<Guid> DeletePatientAsync(Guid patientId);
+    Task<Guid> AddPatientAsync(Patient product);
 }
